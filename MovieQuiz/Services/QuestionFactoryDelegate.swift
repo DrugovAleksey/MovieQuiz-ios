970d40a9a8f42909c8b9975a34ea6fe8f0e18d2b
Aleksey_Drugov <lexa_drugov@mail.ru>
2026-03-21 11:33:31 +0300

    Сделал задания по sprint_06, возможно есть ошибки. Чтобы все проверить сервер был занят (server busy), а времени уже нет

--- MovieQuiz/Services/QuestionFactoryDelegate.swift
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -5,6 +5,10 @@
 //  Created by Flymetric on 22.02.2026.
 //
 
-protocol QuestionFactoryDelegate: AnyObject {
+// если делать как в учебнике то свифт требует
+// protocol QuestionFactoryDelegate: AnyObject {
+protocol QuestionFactoryDelegate {
     func didReceiveNextQuestion(question: QuizQuestion?)
+    func didLoadDataFromServer() // сообщение об успешной загрузке
+    func didFailToLoadData(with error: Error) // сообщение об ошибке загрузки
 }
